24564e18fb7983a06ee8a56bdb2d2787e41eca27
Mark Zammit <91952782+MarkBumber2004@users.noreply.github.com>
2024-03-15 10:52:01 +0100

    Implement Game class containing the core game logic

--- ClockPatience/ClockPatience/Models/Game.cs
+++ ClockPatience/ClockPatience/Models/Game.cs
@@ -1,6 +1,39 @@
-namespace ClockPatience.Models;
-
-public class Game
+namespace ClockPatience.Models
 {
-    
+    public class Game
+    {
+        // List of piles for each rank of cards (Ace to King)
+        private List<Pile> piles = Enumerable.Range(0, 13).Select(_ => new Pile()).ToList();
+
+        // Method to setup the game with a deck of cards
+        public void SetupDeck(Deck deck)
+        {
+            // Distribute the cards in the deck to the corresponding piles based on their rank
+            foreach (var card in deck.GetCards())
+            {
+                int pileIndex = (int)card.Rank - 1;
+                piles[pileIndex].AddCard(card);
+            }
+        }
+
+        // Method to play the game and return the count of exposed cards and the last card
+        public (int exposedCount, Card lastCard) Play()
+        {
+            int exposedCount = 0;
+            // Start with the top card of the King pile
+            Card currentCard = piles[12].GetTopCard();
+
+            // Continue to the pile of the current card's rank until a pile is empty
+            while (currentCard != null)
+            {
+                exposedCount++;
+                int nextPileIndex = (int)currentCard.Rank - 1;
+                if (piles[nextPileIndex].IsEmpty()) break;
+                currentCard = piles[nextPileIndex].GetTopCard();
+            }
+
+            // Return the count of exposed cards and the last card
+            return (exposedCount, currentCard);
+        }
+    }
 }

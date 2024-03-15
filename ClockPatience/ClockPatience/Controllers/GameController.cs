3020f9dfe161b8d589d3d45df2df26fa3425d249
Mark Zammit <91952782+MarkBumber2004@users.noreply.github.com>
2024-03-15 12:46:21 +0100

    Setup GameController with singleton pattern

--- ClockPatience/ClockPatience/Controllers/GameController.cs
+++ ClockPatience/ClockPatience/Controllers/GameController.cs
@@ -1,6 +1,20 @@
-namespace ClockPatience.Controllers;
+using ClockPatience.Models;
+using ClockPatience.Views;
 
-public class GameController
+namespace ClockPatience.Controllers
 {
-    
+    public class GameController
+    {
+        // Singleton instance of GameController
+        private static GameController instance = new GameController();
+        private View view; // View for the game
+        private Game game; // Model for the game
+
+        // Private constructor to enforce singleton pattern
+        private GameController()
+        {
+            view = new View();
+            game = new Game();
+        }
+    }
 }

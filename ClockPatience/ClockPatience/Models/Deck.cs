a98b184870af56a7e22bcfff82aa001b97308693
Mark Zammit <91952782+MarkBumber2004@users.noreply.github.com>
2024-03-15 10:30:53 +0100

    Implementing Deck class for handling a list of cards

--- ClockPatience/ClockPatience/Models/Deck.cs
+++ ClockPatience/ClockPatience/Models/Deck.cs
@@ -1,6 +1,20 @@
-namespace ClockPatience.Models;
-
-public class Deck
+namespace ClockPatience.Models
 {
-    
+    public class Deck
+    {
+        // List of cards in the deck
+        private List<Card> cards;
+
+        // Constructor for the Deck class, requires a list of cards
+        public Deck(List<Card> cards)
+        {
+            this.cards = cards;
+        }
+
+        // Method to add a card to the deck
+        public void AddCard(Card card) => cards.Add(card);
+
+        // Method to get all cards in the deck
+        public IEnumerable<Card> GetCards() => cards;
+    }
 }

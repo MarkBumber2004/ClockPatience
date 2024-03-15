d3d8a0246f1f16b4784b0cfc4f905c64edc8fb17
Mark Zammit <91952782+MarkBumber2004@users.noreply.github.com>
2024-03-15 10:14:21 +0100

    Implementing Pile class to manage a stack of cards

--- ClockPatience/ClockPatience/Models/Pile.cs
+++ ClockPatience/ClockPatience/Models/Pile.cs
@@ -1,6 +1,18 @@
-namespace ClockPatience.Models;
-
-public class Pile
+namespace ClockPatience.Models
 {
-    
+    public class Pile
+    {
+        // Stack of cards in the pile
+        private Stack<Card> cards = new Stack<Card>();
+
+        // Method to add a card to the pile
+        public void AddCard(Card card) => cards.Push(card);
+
+        // Method to get the top card from the pile
+        // Returns null if the pile is empty
+        public Card GetTopCard() => cards.Count > 0 ? cards.Pop() : null;
+
+        // Method to check if the pile is empty
+        public bool IsEmpty() => cards.Count == 0;
+    }
 }
